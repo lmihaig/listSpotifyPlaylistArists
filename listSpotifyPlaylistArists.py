ba8720e18d676d7442f0d7dcea94abbceb1f93c1
lmihaig <licu.mihai21@gmail.com>
2020-11-19 22:39:09 +0200

    init

---
+++ listSpotifyPlaylistArists.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+print("Enter Spotify ID:") 
+playlistId = input()
+print("Enter Token:") 
+token = input()
+response_json = []
+counter = { }
+
+
+numberofItems = requests.get(f"https://api.spotify.com/v1/playlists/{playlistId}/tracks?market=Ro&&fields=total",
+                            headers ={
+                                "Content-Type": "application.json",
+                                "Authorization": f"Bearer {token}"
+                            }
+                            ).json()["total"]    
+iterations = (numberofItems // 100) +1
+
+
+def getNames(offset):
+    url= f"https://api.spotify.com/v1/playlists/{playlistId}/tracks?market=Ro&fields=items(track(artists(name)))&offset={offset}"
+    response = requests.get(url,
+                            headers ={
+                                "Content-Type": "application.json",
+                                "Authorization": f"Bearer {token}"
+                            }
+                            )
+
+    global response_json
+    response_json = response_json + response.json()["items"]
+    
+for i in range(iterations):
+    getNames(i*100)
+   
+for i in response_json:
+    name = i["track"]["artists"][0]["name"]
+    if name in counter.keys():
+        counter[name] +=1
+    else:
+        counter[name] = 1
+
+for i in sorted(counter, key=counter.get, reverse=True):
+    print(i, counter[i])
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Quick python script that outputs all of the artists in a Spotify playlist by occurrence. 
+
+pip install -r requirements.txt
+
+Spotify ID : Right Click Playlist -> Share -> Copy Spotify URI then delete spotify:playlist: part from the string 
+    example : spotify:playlist:1kw9wSUDPWsAEDCGKAMkGU -> 1kw9wSUDPWsAEDCGKAMkGU
+
+Token : https://developer.spotify.com/console/get-playlist-tracks/?playlist_id=21THa8j9TaSGuXYNBU5tsC -> Scroll down and click "GET TOKEN"
