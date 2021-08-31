65a2f0d7109b9335d1bf6a7c561ca2152f517b48
lmihaig <licu.mihai21@gmail.com>
2021-08-31 17:33:04 +0300

    now extracts playlist ID from link, writes to file

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ Quick python script that outputs all of the artists in a Spotify playlist by num
 
 pip install -r requirements.txt
 
-Spotify ID : Right Click Playlist -> Share -> Copy Spotify URI then delete spotify:playlist: part from the string 
-    example : spotify:playlist:1kw9wSUDPWsAEDCGKAMkGU -> 1kw9wSUDPWsAEDCGKAMkGU
+Spotify Playlist Link : Right Click Playlist -> Share -> Copy Link to Playlist
 
 Token : https://developer.spotify.com/console/get-playlist-tracks/?playlist_id=21THa8j9TaSGuXYNBU5tsC -> Scroll down and click "GET TOKEN"
--- listSpotifyPlaylistArists.py
+++ listSpotifyPlaylistArists.py
@@ -1,27 +1,28 @@
 import requests
 import json
 
-print("Enter Spotify ID:") 
-playlistId = input()
-print("Enter Token:") 
+output = open("artists.txt", "w")
+print("Enter Spotify Playlist Link:")
+playlistId = input().split("playlist/")[1].split("?")[0]
+print("Enter Token:")
 token = input()
 response_json = []
-counter = { }
+counter = {}
 
 
 numberofItems = requests.get(f"https://api.spotify.com/v1/playlists/{playlistId}/tracks?market=Ro&&fields=total",
-                            headers ={
-                                "Content-Type": "application.json",
-                                "Authorization": f"Bearer {token}"
-                            }
-                            ).json()["total"]    
-iterations = (numberofItems // 100) +1
+                             headers={
+                                 "Content-Type": "application.json",
+                                 "Authorization": f"Bearer {token}"
+                             }
+                             ).json()["total"]
+iterations = (numberofItems // 100) + 1
 
 
 def getNames(offset):
-    url= f"https://api.spotify.com/v1/playlists/{playlistId}/tracks?market=Ro&fields=items(track(artists(name)))&offset={offset}"
+    url = f"https://api.spotify.com/v1/playlists/{playlistId}/tracks?market=Ro&fields=items(track(artists(name)))&offset={offset}"
     response = requests.get(url,
-                            headers ={
+                            headers={
                                 "Content-Type": "application.json",
                                 "Authorization": f"Bearer {token}"
                             }
@@ -29,16 +30,19 @@ def getNames(offset):
 
     global response_json
     response_json = response_json + response.json()["items"]
-    
+
+
 for i in range(iterations):
     getNames(i*100)
-   
+
 for i in response_json:
     name = i["track"]["artists"][0]["name"]
     if name in counter.keys():
-        counter[name] +=1
+        counter[name] += 1
     else:
         counter[name] = 1
 
 for i in sorted(counter, key=counter.get, reverse=True):
-    print(i, counter[i])
+    output.write(f"{counter[i]} songs from {i}\n")
+
+output.close()
